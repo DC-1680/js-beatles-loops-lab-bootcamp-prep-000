034492cb44c6037734b8f0dda591df8fd3b405cf
Daryl Concha <concha.daryl@gmail.com>
2018-03-15 21:31:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians, instruments){
   var newArr = [];
 
   for (var i = 0; i < musicians.length; i++){
-      newArr.push(musicians[i] + instruments[i])
+      newArr.push(musicians[i] + "plays" + instruments[i])
   }
   return newArr;
 }
